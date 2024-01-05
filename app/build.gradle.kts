141b9f52728416d298381aca87372693b46a2c73
Alessandro <jpinegro8@gmail.com>
2024-01-04 23:58:15 -0500

    [FEAT][JP] add LoginActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,10 @@ android {
         jvmTarget = "1.8"
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
 }
 
 dependencies {
@@ -45,4 +49,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- app/src/main/java/com/app/vales/Login/LoginActivity.kt
+++ app/src/main/java/com/app/vales/Login/LoginActivity.kt
@@ -1,13 +1,27 @@
 package com.app.vales.Login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.vales.MainActivity
 import com.app.vales.R
+import com.app.vales.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var bindig : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        bindig = ActivityLoginBinding.inflate(layoutInflater);
+        setContentView(bindig.root)
 
+        setupView()
+
+
+    }
+    fun setupView() {
+        bindig.btnLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
